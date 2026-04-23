08cb81830cd740582039613d794f2fe8d5a4a4e4
Atharva12-eng <nikamatharva32@gmail.com>
2026-04-24 00:54:28 +0530

    Add files via upload

---
+++ age.py
@@ -0,0 +1,8 @@
+print("Enter your age: ")
+ageinyears=int(input())
+
+ageinmonths = ageinyears*12
+print("Your age in months is: ",ageinmonths)
+
+ageindays = ageinyears*365
+print("Your age in days is : ",ageindays)

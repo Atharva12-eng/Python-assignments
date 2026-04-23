49acd389182f07219c4b550784aaa0c814525020
Atharva12-eng <nikamatharva32@gmail.com>
2026-04-24 00:53:07 +0530

    Add files via upload

---
+++ average.py
@@ -0,0 +1,16 @@
+print("Enter first number:")
+num1=int(input())
+print("Enter second number:")
+num2=int(input())
+print("Enter third number:")
+num3=int(input())
+print("Enter fourth number:")
+num4=int(input())
+print("Enter fifth number:")
+num5=int(input())
+
+Total=num1+num2+num3+num4+num5
+print(Total)
+
+avg=Total/5
+print(avg)

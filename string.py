77d92c0a5b408bd61ed1a80f3c16d8ae1b123c01
Atharva12-eng <nikamatharva32@gmail.com>
2026-04-24 00:55:23 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,10 @@
+name="Welcome"
+print("The first character of the string is:",name[0])
+
+print("The last character of the string is:",name[6])
+
+
+name="Welcome" 
+print("The first character of the string is:",name[-7])
+
+print("The last character of the string is:",name[-1])
